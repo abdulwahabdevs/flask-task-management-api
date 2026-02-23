01fbc7db719ac592d82ead2e5f14f3b68b1c0c62
Abdulwahab Urinboev <abdulwahab@Abdulwahabs-MacBook-Pro.local>
2026-02-23 08:13:44 +0500

    Setup Flask project with dotenv-safe run.py

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .config import Config
+from .extensions import db, migrate, jwt
+import os
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    print("DATABASE_URL =", os.getenv("DATABASE_URL"))
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+
+    return app
